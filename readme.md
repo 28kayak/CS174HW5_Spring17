9e1679b9b11a90b5da57cc54b5f272473a82844b
28kayak <kaya.ota@sjsu.edu>
2017-04-25 15:55:14 -0700

    init

---
+++ readme.md
@@ -0,0 +1,3 @@
+# CS174 HW5
+
+Web site is running at
---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kaya on 4/24/2017.
+ */
+var express = require('express');
+app = express();
+
+
+
+app.get('/', function(req, res){
+
+});
